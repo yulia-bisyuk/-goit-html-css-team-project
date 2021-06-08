183b35199226b30a0b98d9665d92403a80407610
eche-unicorn <d.anasteysha@gmail.com>
2021-06-09 00:48:50 +0300

    reviews interim

--- src/slick/slider.js
+++ src/slick/slider.js
@@ -1,1 +1,9 @@
-$('.reviews-slider').slick();
+$('.reviews-slider').slick({
+  infinite: true,
+	dots: true,
+	arrows: false,
+	autoplay: true,
+	autoplaySpeed: 3000,
+	fade: true,
+	fadeSpeed: 1000
+});
